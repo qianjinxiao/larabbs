4add26fdce4c38c810a4a118731319dd4d449aed
vagrant <vagrant@homestead>
2020-03-18 06:14:11 +0000

    找回密码成功消息提示

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -36,4 +36,16 @@ class ResetPasswordController extends Controller
     {
         $this->middleware('guest');
     }
+
+    /**
+     * 重写sendResetResponse
+     * @param Request $request
+     * @param $response
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
+    protected function sendResetResponse(Request $request, $response)
+    {
+        session()->flash('success', '密码更新成功，您已成功登录！');
+        return redirect($this->redirectPath());
+    }
 }
--- app/Listeners/EmailVerified.php
+++ app/Listeners/EmailVerified.php
@@ -26,7 +26,6 @@ class EmailVerified
      */
     public function handle(Verified $event)
     {
-        dd($event->user->name);
         // 会话里闪存认证成功后的消息提醒
         session()->flash('success', '邮箱验证成功 ^_^');
     }
